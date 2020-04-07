274b40156618660df77741a2a3d390f1ef2c8f35
Otto <61689180+prOttonicFusion@users.noreply.github.com>
2020-04-08 01:36:01 +0300

    implement input file parsing

---
+++ src/IO.cpp
@@ -0,0 +1,61 @@
+/********************************************************************
+ * Functions for handling input & output
+ *******************************************************************/
+#include "IO.hpp"
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+/**
+ * A function for reading in city coordinates from a xyz-file of the
+ * format: 
+ * <number of cities>
+ * comment line
+ * <name> <X> <Y>
+ * ...
+ * 
+ **/
+bool parseXYZFile(int &Ncities, std::string *&cityNames, float *&xpos, float *&ypos)
+{
+  std::ifstream infile("cityCoordinates.xyz");
+  std::string line;
+
+  // On error
+  if (!infile) return false;
+
+  // Read number of cities
+  std::getline(infile, line);
+  std::istringstream iss(line);
+  if (!(iss >> Ncities)) return false;
+
+  // Initialize arrays accordingly
+  cityNames = new std::string[Ncities];
+  xpos = new float[Ncities];
+  ypos = new float[Ncities];
+
+  // Skip comment line
+  std::getline(infile, line);
+
+  // Read rest of file line-by-line
+  int i = 0;
+  while (std::getline(infile, line))
+  {
+    std::istringstream iss(line);
+    std::string name;
+    float x, y;
+
+    // Attempt to parse line
+    if (!(iss >> name >> x >> y))
+    {
+      break;
+    }
+
+    cityNames[i] = name;
+    xpos[i] = x;
+    ypos[i] = y;
+
+    i++;
+  }
+  return true;
+}
---
+++ src/IO.hpp
@@ -0,0 +1,6 @@
+/********************************************************************
+ * Header file for IO.cpp
+ *******************************************************************/ 
+#include <iostream>
+
+bool parseXYZFile(int &Ncities, std::string*& cityNames, float*& xpos, float*& ypos);
