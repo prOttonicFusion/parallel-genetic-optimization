cccf72cc36d283210118a47a3e9d5b32cf9636fb
Otto <61689180+prOttonicFusion@users.noreply.github.com>
2020-04-10 14:30:22 +0300

    refactored genetics header

--- src/genetics.hpp
+++ src/genetics.hpp
@@ -9,14 +9,13 @@
  **/
 class Individ
 {
-  private:
   public:
   float distance; // The squared total distance travelled
   int *route;     // An array containin the visited cities' indices in visiting order
-  Individ();
+  Individ();      // Constructor
+  ~Individ();     // Desturctor
   Individ(int route[], float xpos[], float ypos[], int Ncities);
   void setRoute(int route[], float xpos[], float ypos[], int Ncities);
-  ~Individ();
   std::string getRouteAsString(std::string cityNames[], int Ncities);
 };
 
