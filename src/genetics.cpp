a5e29a3152de5c04e5e1fe0d46b149c49abca7a5
Otto <61689180+prOttonicFusion@users.noreply.github.com>
2020-04-11 01:27:17 +0300

    implement replacing of parents by offspring

--- src/genetics.cpp
+++ src/genetics.cpp
@@ -16,46 +16,51 @@ float distanceBetweenCities(int index1, int index2, float xpos[], float ypos[])
 void breedIndivids(int indexToBreed1, int indexToBreed2, Individ population[], float xpos[], float ypos[], int popSize, int Ncities)
 {
   /** 
-     * Generate offspring genom heuristically:
+     * Generate 2 children genoms heuristically:
      * - Take first city of one parent
      * - The Nth city in the child is chosen from the parents' Nth genes by checking 
      *   which of the two is closer to the child's N-1th city
      * - If the closer city is already found in the child, choose the other
      **/
 
-  int childRoute[Ncities];
+  int childRoutes[2][Ncities];
   Individ parent1 = population[indexToBreed1];
   Individ parent2 = population[indexToBreed2];
 
-  childRoute[0] = parent1.route[0];
+  childRoutes[0][0] = parent1.route[0];
+  childRoutes[0][1] = parent1.route[0];
 
-  for (int i = 1; i < Ncities; i++)
-  {
-    int nextCity;
-    if (std::find(childRoute, childRoute + i, parent1.route[i]) != childRoute + i)
+  for (int k = 0; k < 2; k++)
+    for (int i = 1; i < Ncities; i++)
     {
-      // Found parent1.route[i] in childRoute
-      nextCity = parent2.route[i];
-    }
-    else if (std::find(childRoute, childRoute + i, parent2.route[i]) != childRoute + i)
-    {
-      // Found parent2.route[i] in childRoute
-      nextCity = parent1.route[i];
-    }
-    else
-    {
-      // Neither was found --> choose closest
-      int dist1 = distanceBetweenCities(childRoute[i - 1], parent1.route[i], xpos, ypos);
-      int dist2 = distanceBetweenCities(childRoute[i - 1], parent2.route[i], xpos, ypos);
-      if (dist1 < dist2)
+      int nextCity;
+      if (std::find(childRoutes[k], childRoutes[k] + i, parent1.route[i]) != childRoutes[k] + i)
       {
+        // Found parent1.route[i] in childRoutes
+        nextCity = parent2.route[i];
+      }
+      else if (std::find(childRoutes[k], childRoutes[k] + i, parent2.route[i]) != childRoutes[k] + i)
+      {
+        // Found parent2.route[i] in childRoutes
         nextCity = parent1.route[i];
       }
       else
       {
-        nextCity = parent2.route[i];
+        // Neither was found --> choose closest
+        int dist1 = distanceBetweenCities(childRoutes[k][i - 1], parent1.route[i], xpos, ypos);
+        int dist2 = distanceBetweenCities(childRoutes[k][i - 1], parent2.route[i], xpos, ypos);
+        if (dist1 < dist2)
+        {
+          nextCity = parent1.route[i];
+        }
+        else
+        {
+          nextCity = parent2.route[i];
+        }
       }
+      childRoutes[k][i] = nextCity;
     }
-    childRoute[i] = nextCity;
-  }
+
+    population[indexToBreed1].setRoute(childRoutes[0], xpos, ypos, Ncities);
+    population[indexToBreed2].setRoute(childRoutes[1], xpos, ypos, Ncities);
 }
