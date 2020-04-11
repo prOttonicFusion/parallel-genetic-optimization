4b4153d562ec6069ab84c52a04e14ef0d9dc780e
Otto <61689180+prOttonicFusion@users.noreply.github.com>
2020-04-12 01:49:21 +0300

    fix error with duplicate cities on route

--- src/genetics.cpp
+++ src/genetics.cpp
@@ -28,36 +28,46 @@ Individ breedIndivids(Individ parent1, Individ parent2, float xpos[], float ypos
 
   childRoute[0] = parent1.route[0];
 
-  for (int k = 0; k < 2; k++)
-    for (int i = 1; i < Ncities; i++)
+  for (int i = 1; i < Ncities; i++)
+  {
+    int nextCity;
+    bool parent1CityInChild = std::find(childRoute, childRoute + i, parent1.route[i]) != childRoute + i;
+    bool parent2CityInChild = std::find(childRoute, childRoute + i, parent2.route[i]) != childRoute + i;
+    if (parent1CityInChild && parent2CityInChild)
     {
-      int nextCity;
-      if (std::find(childRoute, childRoute + i, parent1.route[i]) != childRoute + i)
+      // Both parent1.route[i] and parent2.route[i] already found in childRoute
+      nextCity = 0;
+      while (std::find(childRoute, childRoute + i, nextCity) != childRoute + i)
       {
-        // Found parent1.route[i] in childRoutes
-        nextCity = parent2.route[i];
+        nextCity++;
       }
-      else if (std::find(childRoute, childRoute + i, parent2.route[i]) != childRoute + i)
+    }
+    else if (parent1CityInChild)
+    {
+      // Found parent1.route[i] in childRoute
+      nextCity = parent2.route[i];
+    }
+    else if (parent2CityInChild)
+    {
+      // Found parent2.route[i] in childRoute
+      nextCity = parent1.route[i];
+    }
+    else
+    {
+      // Neither was found --> choose closest
+      int dist1 = distanceBetweenCities(childRoute[i - 1], parent1.route[i], xpos, ypos);
+      int dist2 = distanceBetweenCities(childRoute[i - 1], parent2.route[i], xpos, ypos);
+      if (dist1 < dist2)
       {
-        // Found parent2.route[i] in childRoutes
         nextCity = parent1.route[i];
       }
       else
       {
-        // Neither was found --> choose closest
-        int dist1 = distanceBetweenCities(childRoute[i - 1], parent1.route[i], xpos, ypos);
-        int dist2 = distanceBetweenCities(childRoute[i - 1], parent2.route[i], xpos, ypos);
-        if (dist1 < dist2)
-        {
-          nextCity = parent1.route[i];
-        }
-        else
-        {
-          nextCity = parent2.route[i];
-        }
+        nextCity = parent2.route[i];
       }
-      childRoute[i] = nextCity;
     }
+    childRoute[i] = nextCity;
+  }
 
   return Individ(childRoute, xpos, ypos, Ncities);
 }
@@ -68,7 +78,7 @@ void mutateIndivid(int indexToMutate, Individ population[], int Ncities, std::mt
   std::uniform_real_distribution<float> uniformRand(0.0, 1.0);
   int ind1 = (int)(Ncities * uniformRand(rng));
   int ind2 = (int)(Ncities * uniformRand(rng));
-  
+
   // Swap array elements
   int element1 = population[indexToMutate].route[ind1];
   population[indexToMutate].route[ind1] = population[indexToMutate].route[ind2];
--- src/main.cpp
+++ src/main.cpp
@@ -11,11 +11,13 @@
 int main(int argc, char *argv[])
 {
   const int popSize = 50;                 // Size of population
-  const int crossPerIteration = 2;        // Number of individs to crossover each iteration
-  const int maxIterations = 1000000;      // Max. number of iterations if solution doesn't converge
+  const float replaceProportion = 0.5;    // Proportion population be replaced by offspring every iteration
+  const int maxIterations = 100000;       // Max. number of iterations if solution doesn't converge
   const float mutationProbability = 0.05; // The probability of offspring getting mutated
   const int writeToScreenInterval = 100;  // Output data to screen every this many iterations
   const int writeToFileInterval = 100;    // Output data to file every this many iterations
+
+  const int crossPerIter = (int)(replaceProportion * popSize); //Number of crossowers/iteration
 
   // Parse command line arguments
   if (argc < 2)
@@ -65,7 +67,6 @@ int main(int argc, char *argv[])
   }
 
   ////////////////////// Main calculation loop //////////////////////
-  Individ fittest[crossPerIteration];
   int indexToBreed1, indexToBreed2, iterCount = 0;
   bool hasConverged = false;
   while (!hasConverged && iterCount < maxIterations)
@@ -84,17 +85,17 @@ int main(int argc, char *argv[])
     if (iterCount % writeToFileInterval == 0)
       writeToOutputFile(iterCount, population[0].route, bestRouteStr, bestRouteLen, Ncities);
 
-    // ----------------------- Selection ----------------------------
-    // We choose the two fittest individs for breeding
-    indexToBreed1 = 0;
-    indexToBreed2 = 1;
-
     // ------------------------ Breeding ----------------------------
-    // Pair two fittest individs
-    Individ child = breedIndivids(population[0], population[1], xpos, ypos, popSize, Ncities);
-    // Replace the 2 least fit individs in population with the new offspring
-    population[popSize - 1] = child;
-    //population[popSize - 2].setRoute(childRoutes[1], xpos, ypos, Ncities);
+    // Pair a fraction of the population; select parents from the most fit
+    // and replace those less fit with offspring
+    for (int i = 1; i <= crossPerIter; i++)
+    {
+      int parent1 = uniformRand(rng) * 10; // TODO: change hardcoded number to variable
+      int parent2 = uniformRand(rng) * 10;
+      parent2 = (parent1 == parent2) ? parent2 + 1 : parent2;
+      Individ child = breedIndivids(population[parent1], population[parent2], xpos, ypos, popSize, Ncities);
+      population[popSize - i] = child;
+    }
 
     // ------------------------ Mutation ----------------------------
     if (uniformRand(rng) < mutationProbability)
