25cd0fc1de19aab63f639beff4ffdb7d981742f0
Otto <61689180+prOttonicFusion@users.noreply.github.com>
2020-04-13 02:35:13 +0300

    changed to Doxygen commenting

--- src/IO.hpp
+++ src/IO.hpp
@@ -8,11 +8,48 @@
 #include "genetics.hpp"
 #include <iostream>
 
+/**
+ * Parse a xyz-file into an array of City structs
+ *
+ * @param [in] inputFile Path to a input file in xyz-format  
+ * @param [out] Ncities Number of cities in the input file 
+ * @param [out] cities Cities as structs in an array
+ * @return true If parse succeeded
+ * @return flase If an error occured     
+ */
 bool parseXYZFile(std::string inpuFile, int &Ncities, City *&cities);
 
+/**
+ * Write string to the output file
+ *
+ * @param [in] outputString Content to write to the file 
+ * @param [in] overWrite Should the file be overwritten?
+ * @return true If write succeeded
+ * @return flase If an error occured  
+ */
 bool writeToOutputFile(std::string outputString, bool overWrite = false);
+
+/**
+ * Write formatted data to output file
+ * 
+ * @param iterCount [in] Current iteration number
+ * @param bestRoute [in] Best route as int array 
+ * @param bestRouteStr [in] Best route as a string of city names
+ * @param bestRouteLen [in] Length of the best route
+ * @param Ncities [in] Number of cities in one route
+ * @param overWrite [in] SHould we overwrite the file?
+ * @return true If write succeeded
+ * @return flase If an error occured  
+ */
 bool writeToOutputFile(int iterCount, int bestRoute[], std::string bestRouteStr, float bestRouteLen, int Ncities, bool overWrite = false);
 
+/**
+ * Write formatted data to screen
+ * 
+ * @param iterCount [in] Current iteration number
+ * @param bestRouteStr [in] Best route as a string of city names
+ * @param bestRouteLen [in] Length of the best route
+ */
 void writeToScreen(int iterCount, std::string bestRouteStr, float bestRouteLen);
 
 #endif
--- src/individ.hpp
+++ src/individ.hpp
@@ -4,26 +4,65 @@
 
 #ifndef INDIVID_H
 #define INDIVID_H
-#include <iostream>
 #include "city.hpp"
+#include <iostream>
 
 /**
  * Class for storing one solution to the problem, i.e. one route
- * and a corresponding total distance
- **/
+ * and the corresponding total distance
+ * 
+ */
 class Individ
 {
   public:
   float distance; // The squared total distance travelled
   int *route;     // An array containin the visited cities' indices in visiting order
-  Individ();      // Constructor
+
+  /**
+   * Construct a new Individ object
+   */
+  Individ();
+
+  /**
+   * Construct a new Individ object
+   * 
+   * @param route [in] Int array of city indices
+   * @param cities [in] The actual cities as City struct array
+   * @param Ncities [in] Number of cities in route
+   */
   Individ(int route[], City cities[], int Ncities);
-  // ~Individ();     // Desturctor
+
+  /**
+   * Initialize Individ with route. Same as setRoute, but
+   * also allocates the route array
+   * 
+   * @param route [in] Int array of city indices
+   * @param cities [in] The actual cities as City struct array
+   * @param Ncities [in] Number of cities in route
+   */
   void init(int route[], City cities[], int Ncities);
+
+  /**
+   * Set the route of the Individ object & calculate the distance
+   * 
+   * @param route [in] Int array of city indices
+   * @param cities [in] The actual cities as City struct array
+   * @param Ncities [in] Number of cities in route
+   */
   void setRoute(int route[], City cities[], int Ncities);
+
+  /**
+   * Get the route as a string, i.e. all city names on a single line 
+   * 
+   * @param cities [in] The cities as City struct array
+   * @param Ncities [in] Number of cities in route
+   * @return std::string 
+   */
   std::string getRouteAsString(City cities[], int Ncities);
 
-  // Operator overloads
+  /**
+   * Operator overloads
+   */
   friend bool operator>(const Individ &i1, const Individ &i2);
   friend bool operator<(const Individ &i1, const Individ &i2);
   friend bool operator==(const Individ &i1, const Individ &i2);
