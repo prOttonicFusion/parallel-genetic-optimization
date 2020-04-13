e6883ac85696e26fbd4b21868acc9473ae59aeb0
Otto <61689180+prOttonicFusion@users.noreply.github.com>
2020-04-14 00:14:57 +0300

    use distanceBetweenCities() in Individ class

--- src/individ.cpp
+++ src/individ.cpp
@@ -37,14 +37,9 @@ void Individ::setRoute(int route[], City cities[], int Ncities)
   float d = 0.0, xdiff, ydiff;
   for (int i = 1; i < Ncities; i++)
   {
-    // xdiff = (cities[route[i]].xpos - cities[route[i - 1]].xpos);
-    // ydiff = (cities[route[i]].ypos - cities[route[i - 1]].ypos);
-    // d += xdiff * xdiff + ydiff * ydiff;
-    d += distanceBetweenCities();
+    d += distanceBetweenCities(cities[route[i]], cities[route[i-1]]);
   }
-  xdiff = (cities[route[0]].xpos - cities[route[Ncities - 1]].xpos);
-  ydiff = (cities[route[0]].ypos - cities[route[Ncities - 1]].ypos);
-  d += xdiff * xdiff + ydiff * ydiff;
+  d += distanceBetweenCities(cities[route[0]], cities[route[Ncities-1]]);
   this->routeLength = d;
 }
 
