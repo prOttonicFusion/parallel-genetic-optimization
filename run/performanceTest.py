aedbef12b6ec6212c5137ce3b0a87e20fc4665d2
Otto Lindblom <61689180+prOttonicFusion@users.noreply.github.com>
2020-04-28 22:10:04 +0300

    measure both time and route length

--- run/performanceTest.py
+++ run/performanceTest.py
@@ -16,8 +16,8 @@ import subprocess
 import numpy as np
 import matplotlib.pyplot as plt
 
-Nrepeat = 15           # Run program Nrepeat times for each setup & take average
-Nproc = [1, 2, 3, 4, 5, 6]  # The processor setups to use
+Nrepeat = 3           # Run program Nrepeat times for each setup & take average
+Nproc = [1, 2, 3, 4, 5, 6, 7, 8]  # The processor setups to use
 # Command line rguments for the C++ program
 inputArgs = ['randomCities.xyz', '1000', '0', '100']
 
@@ -35,36 +35,30 @@ def getShortestRouteLength():
     lastOutputFrame = tail('output.dat', 5)
     return float(lastOutputFrame[1].split()[1])
 
-
-# Run the program multiple times for each setup and calculate an average wall-time
-# avgTimes_MPI = np.zeros(len(Nproc))
-# avgFinalLength_MPI = np.zeros(len(Nproc))
-# for i, N in enumerate(Nproc):
-#     runCmd = "subprocess.run(['mpiexec', '-n', '{}', '../src/tsp_ga', '{}', '{}', '{}', '{}'])".format(N, *inputArgs)
-#     print(runCmd)
-#     avgTimes_MPI[i] = timeit.timeit(
-#         stmt=runCmd, setup="import subprocess", number=Nrepeat)
-
-# Run the program multiple times for each setup and calculate an average final route length
+# Run the program multiple times for each setup and calculate an average final route length and wall-time
 avgFinalLength_MPI = np.zeros(len(Nproc))
+avgTimes_MPI = np.zeros(len(Nproc))
 for i, N in enumerate(Nproc):
     print('Running on {} CPUs'.format(N))
     routeLengthSum = 0.0
+    timeSum = 0.0
     for j in range(Nrepeat):
-        subprocess.run(['mpiexec', '-n', str(N), '../src/tsp_ga', *inputArgs])
+        runCmd = "subprocess.run(['mpiexec', '-n', '{}', '../src/tsp_ga', '{}', '{}', '{}', '{}'])".format(N, *inputArgs)
+        timeSum += timeit.timeit(stmt=runCmd, setup="import subprocess", number=1)
         routeLengthSum += getShortestRouteLength()
     avgFinalLength_MPI[i] = routeLengthSum/Nrepeat
+    avgTimes_MPI[i] = timeSum/Nrepeat
 
 print(avgFinalLength_MPI)
 
 ############################ Draw figure ############################
-# plt.figure(num=1, figsize=[7, 4])
-# plt.plot(Nproc, avgTimes_MPI, 'ro--')
-# plt.xlabel("Number of processes")
-# plt.ylabel("Wall-time (s)")
-# # plt.legend()
-# plt.savefig("performance.png")
-# plt.show()
+plt.figure(num=1, figsize=[7, 4])
+plt.plot(Nproc, avgTimes_MPI, 'ro--')
+plt.xlabel("Number of processes")
+plt.ylabel("Wall-time (s)")
+# plt.legend()
+plt.savefig("performance_walltime.png")
+plt.show()
 
 plt.figure(num=1, figsize=[7, 4])
 plt.plot(Nproc, avgFinalLength_MPI, 'o--')
@@ -72,5 +66,5 @@ plt.xlabel("Number of processes")
 plt.ylabel("Mean Final Squared Route Length")
 plt.ticklabel_format(axis="y", style="sci", scilimits=(0,0))
 # plt.legend()
-plt.savefig("performance.png")
+plt.savefig("performance_route.png")
 plt.show()
